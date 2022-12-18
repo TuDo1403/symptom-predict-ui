be2696572b7d6cf9088777f96f93a714c7098f75
TuDo1403 <tudo.dev@gmail.com>
2022-12-18 18:24:27 +0700

    update multiple choice

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,63 @@ import { predict } from "../services/predict";
 import { symptoms } from "../const/const";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [symptom, setSymptom] = useState("");
-  const [predictRes, setPredictRes] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [symptom, setSymptom] = useState("");
+    const [predictRes, setPredictRes] = useState("");
 
-  const handleClick = async () => {
-    setLoading(true);
-    const res = await predict(symptom);
-    setPredictRes(res);
-    setLoading(false);
-  };
+    const handleClick = async () => {
+        setLoading(true);
+        const res = await predict(symptom);
+        setPredictRes(res);
+        setLoading(false);
+    };
 
-  return (
-    <Container style={{"width": "300px", "height": "80vh", "position": "relative"}}>
-      <div style={{"position": "absolute", "top" : "50%", "width": "100%"}}>
-        <div style={{"display": "flex", "flexDirection": "column"}}>
-          <Label style={{"margin": "0"}}>Select your symptom: </Label>
-          <Dropdown
-            deburr
-            search
-            selection
-            placeholder="Symptom"
-            onChange={(_, d) => setSymptom(d.value as string)}
-            options={symptoms.map((v) => {
-              return { key: v, text: v, value: v };
-            })}
-          ></Dropdown>
-          <Button disabled={loading} loading={loading} onClick={handleClick} style={{"margin": "20px 0"}}>
-            Predict
-          </Button>
-        </div>
-        <Label>Result: </Label>
-        <Message error={predictRes.includes("Error")} style={{"margin" : "0", "padding": "0", "height": "36px"}}>
-          <p style={{"height": "100%", "lineHeight": "36px", "padding": "0 10px"}}>{predictRes}</p>
-        </Message>
-      </div>
-    </Container>
-  );
+    return (
+        <Container
+            style={{ width: "300px", height: "80vh", position: "relative" }}
+        >
+            <div style={{ position: "absolute", top: "50%", width: "100%" }}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <Label style={{ margin: "0" }}>Select your symptom: </Label>
+                    <Dropdown
+                        deburr
+                        multiple
+                        search
+                        selection
+                        placeholder="Symptom"
+                        onChange={(_, d) => {
+                            console.log((d.value as string[]).join(","));
+                            setSymptom((d.value as string[]).join(","));
+                        }}
+                        options={symptoms.map((v) => {
+                            return { key: v, text: v, value: v };
+                        })}
+                    ></Dropdown>
+                    <Button
+                        disabled={loading}
+                        loading={loading}
+                        onClick={handleClick}
+                        style={{ margin: "20px 0" }}
+                    >
+                        Predict
+                    </Button>
+                </div>
+                <Label>Result: </Label>
+                <Message
+                    error={predictRes.includes("Error")}
+                    style={{ margin: "0", padding: "0", height: "36px" }}
+                >
+                    <p
+                        style={{
+                            height: "100%",
+                            lineHeight: "36px",
+                            padding: "0 10px",
+                        }}
+                    >
+                        {predictRes}
+                    </p>
+                </Message>
+            </div>
+        </Container>
+    );
 }
